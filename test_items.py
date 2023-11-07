4c61494765633dde7e4c719650c74bd7f143f3ce
Artyom Veselov <xxxvanyalive228xxx@gmail.com>
2023-11-08 00:34:35 +0800

    add time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_basket_button_check(browser):
     browser.get(link)
-    time.sleep(2)
+    time.sleep(30)
 
     add_to_basket_btn = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'btn-add-to-basket')))
     assert add_to_basket_btn, '"Add to basket" button was not found'
